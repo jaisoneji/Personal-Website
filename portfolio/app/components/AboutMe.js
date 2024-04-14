6b0eda07586938b33bd0a7a93ec76738d6f4cea9
jaisoneji <jai.soneji@gmail.com>
2024-04-14 20:54:50 +0530

    made some changes for vercel deployment fixes

--- portfolio/app/components/AboutMe.js
+++ portfolio/app/components/AboutMe.js
@@ -25,6 +25,7 @@ export default function AboutMe() {
                             width={40}
                             height={40}
                             className='inline ml-1'
+                            alt="Hello"
                         />
                     </h1>
                 </div>
